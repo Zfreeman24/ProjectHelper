7ac5b7296da6562226937fbe1036f10359ca14b9
zfreeman3 <zjf13@txstate.edu>
2024-04-08 16:53:19 -0500

    Resolved merge

---
+++ projectGeneration.py
@@ -0,0 +1,7 @@
+#This will take in the arguments provided by the website 
+#It will generate the text needed for the website to provide to the user
+#It will take a general argument, making the GPT into a helper
+#Then it will take in the arguments provided based on what the user stated
+#It will then proceed to generate and pass it onto a class that will generate the website.
+
+
